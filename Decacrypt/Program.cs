c3e087b21b5b4d98dc04a38d6bc2a41f6d16bfaf
willthamic <will.hamic@gmail.com>
2016-07-14 12:54:54 -0500

    initial prime functions

--- Decacrypt/Program.cs
+++ Decacrypt/Program.cs
@@ -21,4 +21,31 @@ namespace Decacrypt
             Application.Run(new Form1());
         }
     }
+
+    static class Prime
+    {
+        // Finds a large prime within a range.
+        // I: int n - How many bits the prime is.
+        // O: BigInteger - The large prime. 
+        static public BigInteger FindPrime(int n)
+        {
+            return 0;
+        }
+
+        // Tests if a value is prime.
+        // I: BigInteger n - Value for testing; int k - How thorough the test is. (iterations of testing)
+        // O: Byte - 0 for composite, 1 for prime, 2 for invalid.
+        static private byte Fermat(BigInteger n, int k)
+        {
+            return 0;
+        }
+
+        // Tests if a value is prime.
+        // I: BigInteger n - Value for testing; int k - How thorough the test is. (iterations of testing)
+        // O: Byte - 0 for composite, 1 for prime, 2 for invalid.
+        static private byte MillerRabin(BigInteger n, int k)
+        {
+            return 0;
+        }
+    }
 }
